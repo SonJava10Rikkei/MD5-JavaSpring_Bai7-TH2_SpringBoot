6177150e86b539f86a6f5de94894cb6eef24b0e4
SonJava10Rikkei <sonjapan7@gmail.com>
2023-06-09 09:24:15 +0700

    Initial commit

---
+++ src/main/java/rikkei/academy/Md5JavaSpringBai7Th2SpringBootApplication.java
@@ -0,0 +1,13 @@
+package rikkei.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md5JavaSpringBai7Th2SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Md5JavaSpringBai7Th2SpringBootApplication.class, args);
+	}
+
+}
